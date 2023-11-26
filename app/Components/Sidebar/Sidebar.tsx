44816d5d50f7b0fad3317287bfd3d046f1ba59f5
kyohei-dan <kyohey.dan@gmail.com>
2023-11-27 00:14:23 +0900

    [Fix] サイドバーのmin-widtを変更

--- app/Components/Sidebar/Sidebar.tsx
+++ app/Components/Sidebar/Sidebar.tsx
@@ -85,6 +85,7 @@ function Sidebar() {
 const SidebarStyled = styled.nav<{ collapsed: boolean }>`
   position: relative;
   width: ${(props) => props.theme.sidebarWidth};
+  min-width: ${(props) => props.theme.sidebarWidth};
   background-color: ${(props) => props.theme.colorBg2};
   border: 2px solid ${(props) => props.theme.borderColor2};
   border-radius: 1rem;
